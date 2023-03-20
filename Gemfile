6408e7150a72e37911af348d0ea7a09158c3782b
megane42 <h.kzm.gm@gmail.com>
2023-03-20 18:22:25 +0900

    annotate を導入

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "stimulus-rails"
 gem "bootsnap", require: false
 
 group :development do
+  gem "annotate"
   gem "web-console"
   gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_products_on_name  (name) UNIQUE
+#
 class Product < ApplicationRecord
   enum name: { air_conditioner: "エアコン", light: "LED照明", ecocute: "エコキュート", refrigerator: "電気冷蔵庫" }
 end
