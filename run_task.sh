5f5f2e443ff9e21a943ef7eb1b18b368c6762ee3
megane42 <h.kzm.gm@gmail.com>
2023-03-30 14:44:38 +0900

    任意のコマンドをコンテナ上で実行するスクリプトを作成
    `--platform-os linux --platform-arch arm64` が、既存の環境のものを尊重してくれないので、手動で追加の必要があった（コントリビュートチャンス）

---
+++ run_task.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+cd $(dirname $0) # どこから実行してもこのスクリプトの位置がカレントディレクトリになるようにする
+
+usage_exit() {
+  echo "Usage: AWS_PROFILE=fukusuke $0 -e {staging,production} -c {command}" 1>&2
+  exit 1
+}
+
+# parse options
+while getopts 'e:c:' opt; do
+  case "${opt}" in
+    e) environment="${OPTARG}" ;;
+    c) command="${OPTARG}" ;;
+    *) usage_exit ;;
+  esac
+done
+
+# check required options
+if [[ -z "${environment}" ]]; then
+  usage_exit
+fi
+if [[ -z "${command}" ]]; then
+  usage_exit
+fi
+
+# ------------------------------------------------------------------------------------------
+
+template=$(copilot task run --generate-cmd fukusuke/${environment}/web 2>&1)
+
+# remove existing --entrypoint and --command
+template=$(echo "${template}" | grep -v -- --entrypoint)
+template=$(echo "${template}" | grep -v -- --command)
+
+# append our --entrypoint and --command
+template="${template} --entrypoint ''"
+template="${template} --command '${command}'"
+
+# stream the logs
+template="${template} --follow"
+
+# specify platform
+template="${template} --platform-os linux --platform-arch arm64"
+
+eval "${template}"
