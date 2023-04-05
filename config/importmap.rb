a77b2b09897c98f279426d75f1cc705a0b87249e
megane42 <h.kzm.gm@gmail.com>
2023-04-05 18:49:38 +0900

    bin/importmap pin countup.js --from unpkg

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,4 @@
 pin "application", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "countup.js", to: "https://unpkg.com/countup.js@2.6.0/dist/countUp.min.js"
