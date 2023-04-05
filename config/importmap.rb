8862c56c24056773f6fffc45e246e1ddbb3c952d
megane42 <h.kzm.gm@gmail.com>
2023-04-05 19:53:25 +0900

    bin/rails stimulus:install

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,6 @@ pin "application", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "countup.js", to: "https://unpkg.com/countup.js@2.6.0/dist/countUp.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
