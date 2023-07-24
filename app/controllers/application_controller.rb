4609a0191f74178ca125801aabf3e3881213eb7d
megane42 <h.kzm.gm@gmail.com>
2023-07-24 18:40:34 +0900

    git grep -l 'FUKUSUKE' | xargs sed -i '' -e 's/FUKUSUKE/HAMASUKE/g'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV["FUKUSUKE_BASIC_AUTH_ID"], password: ENV["FUKUSUKE_BASIC_AUTH_PW"]
+  http_basic_authenticate_with name: ENV["HAMASUKE_BASIC_AUTH_ID"], password: ENV["HAMASUKE_BASIC_AUTH_PW"]
 end
